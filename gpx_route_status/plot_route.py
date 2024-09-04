a6e96bbf605fd3b79aa6d8963c8310f5d652a676
dc-aichara <dcaak7@gmail.com>
2024-09-04 19:48:45 +0900

    misc: Improve closed road highlights in map

--- gpx_route_status/plot_route.py
+++ gpx_route_status/plot_route.py
@@ -10,7 +10,7 @@ MAPBOX_STYLE = config("MAPBOX_STYLE")
 
 
 def plot_route_with_closed_sections(
-    gpx_file_path: str, closed_sections: list
+    gpx_file_path: str, closed_sections: list, distance_threshold: float = 60
 ) -> go.Figure:
     """
     Plots a GPX route with closed sections highlighted.
@@ -23,6 +23,8 @@ def plot_route_with_closed_sections(
         gpx_file_path (str): The file path to the GPX file.
         closed_sections (list): A list of tuples, where each tuple contains the
             start and end coordinates (latitude, longitude) of a closed section.
+        distance_threshold (float): The distance threshold (in meter) to check
+            closed sections.
 
     Returns:
         go.Figure: A Plotly figure object with the route and closed sections
@@ -43,6 +45,10 @@ def plot_route_with_closed_sections(
     )
 
     # Highlight closed sections
+    # Closed section should have start and end to highlight.
+    closed_sections = [
+        section for section in closed_sections if len(section) == 2
+    ]
     for start, end in closed_sections:
         closed_latitudes = []
         closed_longitudes = []
@@ -51,17 +57,18 @@ def plot_route_with_closed_sections(
         for lat, lon in coords:
             if not start_found:
                 if (
-                    geodesic(start[::-1], (lat, lon)).meters < 50
+                    geodesic(start[::-1], (lat, lon)).meters
+                    < distance_threshold
                 ):  # Correct order: (latitude, longitude)
                     start_found = True
-                if geodesic(end[::-1], (lat, lon)).meters < 50:
+                if geodesic(end[::-1], (lat, lon)).meters < distance_threshold:
                     start_found = True
                     end = start
             if start_found:
                 closed_latitudes.append(lat)
                 closed_longitudes.append(lon)
                 if (
-                    geodesic(end[::-1], (lat, lon)).meters < 50
+                    geodesic(end[::-1], (lat, lon)).meters < distance_threshold
                 ):  # Correct order: (latitude, longitude)
                     end_found = True
                     break
@@ -82,17 +89,6 @@ def plot_route_with_closed_sections(
         mapbox_zoom=10,
         mapbox_center={"lat": np.mean(latitudes), "lon": np.mean(longitudes)},
         height=800,
-        # title={
-        #     'text': "GPX Route with Closed Sections Highlighted",
-        #     'y': 0.9,
-        #     'x': 0.5,
-        #     'xanchor': 'center',
-        #     'yanchor': 'top',
-        #     'font': {
-        #         'color': 'blue',
-        #         'size': 24
-        #     }
-        # },
         legend={
             "x": 0.01,
             "y": 0.99,
